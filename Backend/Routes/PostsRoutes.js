0a7ad88edd3aa3e3a03df6727cc061de454a1173
Afshin1992 <mr.choganian@gmail.com>
2021-01-22 16:39:34 -0800

    final

---
+++ Backend/Routes/PostsRoutes.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router(); 
+
+
+const postController = require('../Controllers/PostController');
+
+
+router.get('/', postController.baseRoute);
+
+
+router.post('/create', postController.createPost);
+
+
+router.get('/getPosts', postController.getPosts);
+
+
+router.get('/getPost/:id', postController.getSinglePost);
+
+
+router.put('/post/:id/update', postController.updatePost);
+
+
+router.delete('/delete/:id', postController.deletePost);
+
+module.exports = router;
